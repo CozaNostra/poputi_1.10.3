0bbcdc2d33908d52c52f48065b131f9d9d145319
CozaNostra <cozanostra.me@ya.ru>
2014-05-05 22:35:17 +0400

    install edit to 1.10.3

--- components/poputi/install.php
+++ components/poputi/install.php
@@ -1,12 +1,4 @@
 <?php
-/*********************************************************************************************/
-//																							 //
-//                               InstantVideo v1.5.2 (c) 2011                                //
-//	 					  http://www.instantvideo.ru/, fuze@instancms.ru                     //
-//                              The illegal use is punishable by law                         //
-//                                written by Igor V Bessmeltseff                             //
-//                                                                                           //
-/*********************************************************************************************/
 
     function info_component_poputi(){
 
@@ -18,9 +10,9 @@
         $_component['link']         = 'poputi';
         $_component['author']       = 'cozanostra.me@ya.ru';
         $_component['internal']     = '0';
-        $_component['version']      = '1.10';
+        $_component['version']      = '1.10.3';
 
-		$_component['config'] = cms_model_poputi::getConfig();
+		$_component['config'] 		= cms_model_poputi::getConfig();
 
         return $_component;
 
@@ -32,30 +24,15 @@
 
         $inCore = cmsCore::getInstance();
         $inDB   = cmsDatabase::getInstance();
-        $inConf = cmsConfig::getInstance();
-        include($_SERVER['DOCUMENT_ROOT'].'/includes/dbimport.inc.php');
-		
-		$d = file ($_SERVER['DOCUMENT_ROOT'].'/components/poputi/install.sql'); 
-		$str = implode ("", $d);
-		$queries = explode (";", $str); 
-		foreach ($queries as $q) { 
-			$q = str_replace('#_',$inConf->db_prefix,$q);
-			if(trim($q))
-			{
-				$inDB->query($q);
-			}
-		}
+		$inDB->importFromFile($_SERVER['DOCUMENT_ROOT'].'/components/poputi/install.sql');
+
         return true;
     }
 
 // ========================================================================== //
 
     function upgrade_component_poputi(){
-	$inCore = cmsCore::getInstance();
-        $inDB   = cmsDatabase::getInstance();
-		$inDB->query("UPDATE `cms_components` SET `system` = '0' WHERE `link` = 'poputi';");
         return true;
-        
     }
 
 // ========================================================================== //
